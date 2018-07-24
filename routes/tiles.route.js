c837640cc3094957b22d99225d38d9b32fbf9411
Blair Jordan <B.Jordan@ndy.com>
2018-07-24 15:15:31 +1000

    Get and post from tile js now fetches from tile endpoint

--- routes/tiles.route.js
+++ routes/tiles.route.js
@@ -7,4 +7,29 @@ router.get('/', function (req, res) {
   res.render('tiles');
 });
 
+let demoImg = 'http://labs.pegleg.com.au/images/grid_1K.png';
+
+router.get('/all', function (req, res) {
+  res.json([
+    {i: 0, x: 0, y: 0, url: demoImg, selected: false},
+    {i: 1, x: 4, y: 4, url: demoImg, selected: false},
+    {i: 2, x: 6, y: 8, url: demoImg, selected: false},
+    {i: 3, x: 7, y: 3, url: demoImg, selected: false},
+    {i: 4, x: 3, y: 5, url: demoImg, selected: false},
+    {i: 5, x: 8, y: 6, url: demoImg, selected: false},
+    {i: 6, x: 6, y: 8, url: demoImg, selected: false},
+    {i: 5, x: 4, y: -3, url: demoImg, selected: false},
+    {i: 8, x: -4, y: -2, url: demoImg, selected: false},
+    {i: 9, x: -1, y: 1, url: demoImg, selected: false},
+    {i: 10, x: 1, y: -1, url: demoImg, selected: false},
+    {i: 11, x: 1, y: 1, url: demoImg, selected: false},
+    {i: 12, x: -1, y: -1, url: demoImg, selected: false},
+    {i: 13, x: -2, y: -1, url: demoImg, selected: false}
+  ]);
+});
+
+router.post('/make', function (req, res) {
+  res.json(req.body);
+});
+
 export default router;
