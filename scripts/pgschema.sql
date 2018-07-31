0128841f04d5d644b2667fd052e96bafa9a4e21e
Blair Jordan <B.Jordan@ndy.com>
2018-07-31 12:59:25 +1000

    Modified tables ... and stuff.

--- scripts/pgschema.sql
+++ scripts/pgschema.sql
@@ -1,52 +1,51 @@
-
 CREATE TABLE item_type
 (
- item_type_id BIGSERIAL PRIMARY KEY,
- name         VARCHAR(100) NOT NULL ,
- description  TEXT
+ item_type_id  BIGSERIAL PRIMARY KEY,
+ name          VARCHAR(100) NOT NULL ,
+ description   TEXT
 );
 
 CREATE TABLE player
 (
- player_id  BIGSERIAL PRIMARY KEY,
- username VARCHAR(18) NOT NULL ,
- email    VARCHAR(100)
-);
-
-CREATE TABLE trade
-(
- trade_id  BIGSERIAL PRIMARY KEY,
- source_id  BIGINT NOT NULL REFERENCES player (player_id),  
- target_id BIGINT NOT NULL REFERENCES player (player_id),
- status   VARCHAR(15)  NOT NULL 
+ player_id   BIGSERIAL PRIMARY KEY,
+ username    VARCHAR(18) NOT NULL ,
+ email       VARCHAR(100)
 );
 
 CREATE TABLE item
 (
- item_id      BIGSERIAL PRIMARY KEY,
- item_type_id BIGINT NOT NULL REFERENCES item_type (item_type_id),
- name         VARCHAR(100) NOT NULL ,
- created      TIMESTAMP  DEFAULT CURRENT_TIMESTAMP,
- modified     TIMESTAMP  DEFAULT CURRENT_TIMESTAMP,
- description  TEXT 
+ item_id       BIGSERIAL PRIMARY KEY,
+ item_type_id  BIGINT NOT NULL REFERENCES item_type (item_type_id),
+ name          VARCHAR(100) NOT NULL ,
+ created       TIMESTAMP  DEFAULT CURRENT_TIMESTAMP,
+ modified      TIMESTAMP  DEFAULT CURRENT_TIMESTAMP,
+ description   TEXT 
 );
 
 CREATE TABLE player_item
 (
  player_item_id BIGSERIAL PRIMARY KEY ,
- item_id      BIGINT NOT NULL REFERENCES item (item_id),
- player_id    BIGINT NOT NULL REFERENCES player (player_id),
- item_count   INTEGER NOT NULL ,
- created      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
- modified     TIMESTAMP  DEFAULT CURRENT_TIMESTAMP
+ item_id        BIGINT NOT NULL REFERENCES item (item_id),
+ player_id      BIGINT NOT NULL REFERENCES player (player_id),
+ item_count     INTEGER NOT NULL ,
+ created        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+ modified       TIMESTAMP  DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE offer
 (
- offer_id     BIGSERIAL  PRIMARY KEY,
- trade_id     BIGINT NOT NULL REFERENCES trade (trade_id),
- item_id BIGINT NOT NULL REFERENCES item (item_id),
- item_count   INT NOT NULL
+ offer_id        BIGSERIAL PRIMARY KEY,
+ source_id       BIGINT NOT NULL REFERENCES player (player_id),  
+ target_id       BIGINT NOT NULL REFERENCES player (player_id),
+ target_status   VARCHAR(15)  NOT NULL 
+);
+
+CREATE TABLE offer_item
+(
+ offer_item_id BIGSERIAL  PRIMARY KEY,
+ offer_id      BIGINT NOT NULL REFERENCES offer (offer_id),
+ item_id       BIGINT NOT NULL REFERENCES item (item_id),
+ item_count    INT NOT NULL
 );
 
 INSERT INTO player(username) VALUES ('blair');
@@ -58,10 +57,14 @@ INSERT INTO item_type(name) VALUES ('fish');
 INSERT INTO item(item_type_id, name) VALUES (1, 'mullet');
 INSERT INTO item(item_type_id, name) VALUES (1, 'catfish');
 
-INSERT INTO trade(source_id, target_id, status) VALUES (1, 2, 'O');
+INSERT INTO offer(source_id, target_id, status) VALUES (1, 2, 'O');
+INSERT INTO offer(source_id, target_id, status) VALUES (2, 1, 'O');
+
+INSERT INTO offer_item(offer_id, item_id, item_count) VALUES (1,1,20);
+INSERT INTO offer_item(offer_id, item_id, item_count) VALUES (1,2,5);
+INSERT INTO offer_item(offer_id, item_id, item_count) VALUES (2,2,10);
 
 /*
-
 select
 	p1.username as source,
     p2.username as target,
@@ -69,13 +72,13 @@ select
     o.item_count,
     i.name
 from
-    trade t,
-    offer o, 
+    offer t,
+    offer_item o, 
     item i, 
     player p1,
     player p2
     WHERE p1.player_id = t.source_id
     AND p2.player_id = t.target_id
-    AND t.trade_id = o.trade_id
+    AND t.offer_id = o.offer_id
     AND o.item_id = i.item_id;
 */
