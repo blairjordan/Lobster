161aa19268251294a34dbb0c623dedccfae7c6a2
Blair Jordan <B.Jordan@ndy.com>
2018-08-06 09:17:07 +1000

    Added item add to model and test.

--- test/test_items.js
+++ test/test_items.js
@@ -2,8 +2,20 @@ const assert = require('assert');
 import Item from '../models/item.model';
 
 describe('Item model tests', () => {
-    
-    // TODO: Create item
+    it('Creates an item', (done) => {
+        Item.addItem({name: 'nutella', type: 'fish', description: 'It is nutty and chocolatty.'})
+        .then(function (id) {
+            assert(typeof id !== 'undefined');
+            done();
+        })
+        .catch(function (e) {
+            done(e);
+        });
+    });
+
+    // TODO: Update item
+
+    // TODO: Delete
 
     it('Retrieves all items', (done) => {
         Item.getAll()
