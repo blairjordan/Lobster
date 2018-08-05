f58d0173ebfca9b7c6fa432819c22d32d09b35f3
Blair <blair.j.jordan@gmail.com>
2018-08-05 21:51:39 +1000

    Just added item insert.

--- models/item.model.js
+++ models/item.model.js
@@ -3,7 +3,18 @@ import {db} from '../db/connect';
 let Item = {};
 
 Item.getAll = async () => {
-  return db.any(`select * from item`);
+  return await db.any(`SELECT * FROM item`);
+};
+
+Item.addItem = async (options) => {
+  const {type, name, description} = options;
+  return db.one('INSERT INTO item(item_type_id, name, description) VALUES($1, $2, $3) RETURNING item_id', [type, name, description])
+    .then(id => {
+        return id;
+    })
+    .catch(error => {
+        throw error;
+    });
 };
 
 export default Item;
